73f0c8b823a82380b6a471f172a76b1bdbd54c92
batval <karampish@gmail.com>
2019-10-31 12:15:15 +0300

    Create simple webapp

--- src/main/java/webapp/mvc/excelpdf/ExcelDocument.java
+++ src/main/java/webapp/mvc/excelpdf/ExcelDocument.java
@@ -1,4 +1,56 @@
 package webapp.mvc.excelpdf;
 
-public class ExcelDocument {
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.springframework.web.servlet.view.document.AbstractExcelView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.List;
+
+public class ExcelDocument extends AbstractExcelView {
+    @Override
+    protected void buildExcelDocument (Map<String,Object> model, HSSFWorkbook workbook, HttpServletRequest request, HttpServletResponse response) throws Exception{
+        HSSFSheet excelSheet = workbook.createSheet("Simple Excel example");
+
+        response.setHeader("Content-Disposition","attachment; filename=excelDocument.xls");
+        HSSFFont font = workbook.createFont();
+        font.setFontName("Arial");
+        font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+        font.setColor(HSSFColor.WHITE.index);
+
+        CellStyle styleHeader = workbook.createCellStyle();
+        styleHeader.setFillForegroundColor(HSSFColor.BLUE.index);
+        styleHeader.setFillPattern(CellStyle.SOLID_FOREGROUND);
+        styleHeader.setFont(font);
+
+        setExcelHeader(excelSheet,styleHeader);
+
+        List<Cat> catList = (List<Cat>) model.get("modelObject");
+
+        int rowCount=1;
+        for (Cat cat:catList){
+            HSSFRow row = excelSheet.createRow(rowCount++);
+            row.createCell(0).setCellValue(cat.getName());
+            row.createCell(1).setCellValue(cat.getWeight());
+            row.createCell(2).setCellValue(cat.getColor());
+        }
+    }
+        public void setExcelHeader(HSSFSheet excelSheet, CellStyle styleHeader){
+        HSSFRow header = excelSheet.createRow(0);
+        header.createCell(0).setCellValue("Name");
+        header.getCell(0).setCellStyle(styleHeader);
+        header.createCell(1).setCellValue("Weight");
+        header.getCell(1).setCellStyle(styleHeader);
+        header.createCell(2).setCellValue("Color");
+        header.getCell(2).setCellStyle(styleHeader);
+        }
+
+
+
 }
